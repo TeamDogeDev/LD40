e0db35f455c81ee160182575a2fb8cd7ef564cc6
meisterfuu <info@meister-fuu.de>
2017-12-03 13:57:30 +0100

    Added asteroid sprites

--- core/src/de/dogedev/ld40/assets/enums/Textures.java
+++ core/src/de/dogedev/ld40/assets/enums/Textures.java
@@ -4,6 +4,8 @@ public enum Textures {
     BASE("base.png"),
     PLAYER("player.png"),
     BULLET("bullet.png"),
+    ASTEROID_1("asteroid1.png"),
+    ASTEROID_2("asteroid2.png"),
     ENEMY("enemy.png");
 
     public String name;
