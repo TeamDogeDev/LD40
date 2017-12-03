5c0a7315c9a6d140a8109015ec0b1abfb55deff9
Elektropapst <elektropapst@user.noreply.github.com>
2017-12-03 18:37:16 +0100

    Outline Texture

--- core/src/de/dogedev/ld40/assets/enums/Textures.java
+++ core/src/de/dogedev/ld40/assets/enums/Textures.java
@@ -2,7 +2,7 @@ package de.dogedev.ld40.assets.enums;
 
 public enum Textures {
     BASE("base.png"),
-    PLAYER("player_outline.png"),
+    PLAYER("player_outline_blur.png"),
     BULLET("bullet.png"),
     ASTEROID_1("asteroid1.png"),
     ASTEROID_2("asteroid2.png"),
