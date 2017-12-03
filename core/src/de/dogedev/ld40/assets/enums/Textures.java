53dc840ae4f1289f472a0680e6dee42645ae40e7
meisterfuu <info@meister-fuu.de>
2017-12-03 18:32:02 +0100

    even more

--- core/src/de/dogedev/ld40/assets/enums/Textures.java
+++ core/src/de/dogedev/ld40/assets/enums/Textures.java
@@ -3,7 +3,7 @@ package de.dogedev.ld40.assets.enums;
 public enum Textures {
     BASE("base.png"),
     PLAYER("player_outline.png"),
-    BULLET("bullet.png"),
+    BULLET("bullet_outline.png"),
     ASTEROID_1("asteroid1.png"),
     ASTEROID_2("asteroid2.png"),
     ENEMY("enemy.png"),
