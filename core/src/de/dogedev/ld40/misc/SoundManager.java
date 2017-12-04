6a8e0704e4371c9db6247236cd1552ce7b87a7b4
meisterfuu <info@meister-fuu.de>
2017-12-04 14:34:15 +0100

    Added music and shot sound

--- core/src/de/dogedev/ld40/misc/SoundManager.java
+++ core/src/de/dogedev/ld40/misc/SoundManager.java
@@ -2,6 +2,7 @@ package de.dogedev.ld40.misc;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 
 /**
  * Created by meisterfuu.
@@ -12,6 +13,8 @@ public class SoundManager {
     private static Music engineStart2;
     private static Music engineRunning;
     private static Music engineSlowdown;
+    private static Music music;
+    private static Sound shot;
 
     public static boolean engineOn = false;
 
@@ -20,7 +23,24 @@ public class SoundManager {
         engineRunning = Gdx.audio.newMusic(Gdx.files.internal("sounds/101278__tomlija__air-pump-drone-and-hum.ogg"));
         engineStart = Gdx.audio.newMusic(Gdx.files.internal("sounds/98630__robinhood76__01820-futuristic-space-foley.ogg"));
         engineStart2 = Gdx.audio.newMusic(Gdx.files.internal("sounds/98630__robinhood76__01820-futuristic-space-foley_end.ogg"));
+        music = Gdx.audio.newMusic(Gdx.files.internal("sounds/the_calling_loop.mp3"));
+        shot = Gdx.audio.newSound(Gdx.files.internal("sounds/shot.ogg"));
+
         engineSlowdown.setVolume(0.17f);
+    }
+
+    public static void playShotSound(){
+        shot.play();
+    }
+
+    public static void startMusic(){
+        music.setVolume(0.2f);
+        music.play();
+        music.setLooping(true);
+    }
+
+    public static void stopMusic(){
+        music.stop();
     }
 
     public static void startEngine(){
@@ -43,7 +63,7 @@ public class SoundManager {
             if(engineStart.getPosition() > 5){
                 engineStart2.setVolume(1);
             } else {
-                engineStart2.setVolume(0.17f);
+                engineStart2.setVolume(0.4f);
             }
             engineStart2.play();
 //            engineStart.stop();
